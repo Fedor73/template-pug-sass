ef8036e3a8efec0c402955430acb7b86ec603d32
vinetskii.fedor <vinetskii.fedor@mediasoft.team>
2018-10-30 12:22:52 +0400

    initital setting

--- src/js/external.js
+++ src/js/external.js
@@ -4,3 +4,4 @@
 @@include('external/modernizr-custom.js')
 @@include('external/ofi.min.js')
 @@include('node_modules/jquery/dist/jquery.min.js')
+@@include('node_modules/slick-carousel/slick/slick.min.js')
